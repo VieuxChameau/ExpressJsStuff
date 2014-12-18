4732b105ea636f9c268f2ba0b53098a07bcb6381
VieuxChameau <VieuxChameau@users.noreply.github.com>
2014-12-18 22:12:51 +0100

    Use of static middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/cities', function (request, response) {
 	var cities = ['Caspiana', 'Indigo', 'Paradise'];
 	response.send(cities);
