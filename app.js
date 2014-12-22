65965cb41e4350e876231096d0885494a4d01731
VieuxChameau <VieuxChameau@users.noreply.github.com>
2014-12-22 19:57:29 +0100

    Chapter 5.1 - refactoring route by chain call

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('./logger');
 var bodyParser = require('body-parser');
 
 
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
 
 app.use(express.static('public'));
 
@@ -19,58 +19,59 @@ var cities = {
 	'Flotilla': 'Bustling urban oasis'
 };
 
-app.param('name', function(request, response, next){
+app.param('name', function (request, response, next) {
 	request.cityName = parseCityName(request.params.name);
 	next();
 });
 
 // citites?search=Paradise
-app.get('/cities', function(request, response){
-	if (request.query.search) {
-		response.send(citySearch(request.query.search));
-	}
-	response.send(cities);
-});
-
-app.get('/cities/:name', function (request, response) {
-	var cityInfo = cities[request.cityName];
-	if (cityInfo){
-		response.json(cityInfo);
-	} else {
-		response.status(404).json("City not found");
-	}
-});
+app.route('/cities').
+	get(function (request, response) {
+		if (request.query.search) {
+			response.send(citySearch(request.query.search));
+		}
+		response.send(cities);
+	}).
+	post(parseUrlencoded, function (request, response) {
+		var description = request.body.description;
+		if (description.length > 4) {
+			var city = createCity(request.body.name, description);
+			response.status(201).json(city);
+		} else {
+			response.status(400).json('Invalid City');
+		}
+	});
 
 app.get('/locations', function (request, response) {
 	response.redirect(301, '/cities');
 });
 
 
-app.post('/cities', parseUrlencoded, function (request, response) {
-	var description = request.body.description;
-	if (description.length > 4){
-		var city = createCity(request.body.name, description);
-		response.status(201).json(city);
-	} else {
-		response.status(400).json('Invalid City');
-	}
-});
+app.route('/cities/:name').
+	get(function (request, response) {
+		var cityInfo = cities[request.cityName];
+		if (cityInfo) {
+			response.json(cityInfo);
+		} else {
+			response.status(404).json("City not found");
+		}
+	}).
+	delete(function (request, response) {
+		if (cities[request.cityName]) {
+			delete cities[request.cityName];
+			response.sendStatus(200);
+		} else {
+			response.sendStatus(404);
+		}
+	});
 
-app.delete('/cities/:name', function (request, response){
-	if (cities[request.cityName]){
-		delete cities[request.cityName];
-		response.sendStatus(200);
-	} else {
-		response.sendStatus(404);
-	}
-});
 
-var createCity = function(name, description){
+var createCity = function (name, description) {
 	cities[name] = description;
 	return name;
 };
 
-function citySearch (keyword) {
+function citySearch(keyword) {
 	for (var city in cities) {
 		if (city == keyword) {
 			return cities[keyword];
@@ -79,7 +80,7 @@ function citySearch (keyword) {
 	return null;
 }
 
-function parseCityName(name){
+function parseCityName(name) {
 	return name[0].toUpperCase() + name.slice(1).toLowerCase();
 }
 
